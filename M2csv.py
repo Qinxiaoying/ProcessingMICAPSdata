8a44c07156e7bf8eef3ebb20f1b53541e7db4841
Qinxiaoyinng <758831425@qq.com>
2017-02-27 22:49:39 +0800

    do1

---
+++ M2csv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import pandas as pd
+
+InputDir = ''
+OututDir = ''
+
+
+def ReadData():
+    pass
+
+
+def Write2CSV():
+    pass
+
+if __name__ = "main":
+    pass
